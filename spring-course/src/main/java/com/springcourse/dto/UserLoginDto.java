a097b4e00078b75a4f24e94d1fe5b50738193132
Karine <karinedovallesore@gmail.com>
2020-11-04 14:12:29 -0300

    Criando o controller do user e o dto do login

---
+++ spring-course/src/main/java/com/springcourse/dto/UserLoginDto.java
@@ -0,0 +1,36 @@
+package com.springcourse.dto;
+
+public class UserLoginDto {
+	
+	private String email;
+	
+	private String password;
+
+	public UserLoginDto() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public UserLoginDto(String email, String password) {
+		super();
+		this.email = email;
+		this.password = password;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+}
